ef75b773335b78a2c2c118395c2501ff685cefb2
Naynesh Rathod <nayneshrathod@gmail.com>
2020-05-18 14:28:51 +0530

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppRoutingModule} from './app-routing.module';
+
+
+import {AppComponent} from './app.component';
+import {HomeComponent} from './Components/home/home.component';
+import {HeaderComponent} from './Components/header/header.component';
+import {FooterComponent} from './Components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
